8f93e9669265974049f9fc526e585fa22b4422a7
aRdiiaNaRdiiaN04 <youngcreative12@gmail.com>
2020-09-28 21:02:27 +0200

    MovieSeatBookingApp

--- 01 Form Validator/script.js
+++ 01 Form Validator/script.js
@@ -2,7 +2,7 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+const password2 = document.getElementById('pasword');
 
  function showError(input, message){
       const formControl = input.parentElement;
