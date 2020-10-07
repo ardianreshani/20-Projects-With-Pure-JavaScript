37c84c4424d48434d2779cc8c3da7f4f159927af
aRdiiaNaRdiiaN04 <youngcreative12@gmail.com>
2020-10-07 18:07:26 +0200

    fishin

--- 13 Text-Speech-Reader/script.js
+++ 13 Text-Speech-Reader/script.js
@@ -70,7 +70,47 @@ function createBox(item){
     <img src="${image}" alt="${text}" />
     <p class="info"> ${text}</p>
   `;
-
+  box.addEventListener('click', () => {
+    setTextMessage(text);
+    speakText();
+    box.classList.add('active');
+    setTimeout(() => box.classList.remove('active'),800);
+  })
   main.appendChild(box);
 }
 
+const message = new SpeechSynthesisUtterance();
+
+
+let voices = [];
+
+function getVoices(){
+  voices = speechSynthesis.getVoices();
+  voices.forEach( voice => {
+    const option = document.createElement('option');
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+    voicesSelect.appendChild(option);
+  })
+}
+
+function setTextMessage(text){
+  message.text = text;
+}
+
+function speakText(){
+  speechSynthesis.speak(message);
+}
+function setVoice(e) {
+  message.voice = voices.find(voice => voice.name === e.target.value);
+}
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+toggleBtn.addEventListener('click', () => document.getElementById('text-box').classList.toggle('show'));
+closeBtn.addEventListener('click', () => document.getElementById('text-box').classList.remove('show'));
+voicesSelect.addEventListener('change', setVoice);
+
+readBtn.addEventListener('click', () => {
+  setTextMessage(textarea.value);
+  speakText();
+})
+getVoices();
