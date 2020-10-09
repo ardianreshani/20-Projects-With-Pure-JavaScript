c5ff9d403fd22625009afacf1d89f01ec142a7d1
aRdiiaNaRdiiaN04 <youngcreative12@gmail.com>
2020-10-09 20:39:21 +0200

    preject14

--- 14 Memory Cards/script.js
+++ 14 Memory Cards/script.js
@@ -50,8 +50,31 @@ function createCard(data,index){
     cardEl.push(card);
 
     cardsContainer.appendChild(card);
+    updateCurrentText();
+}
+
+function updateCurrentText(){
+  curremtEl.innerHTML = `${currentActiveCard +1}/${cardEl.length}`;
 }
 
 createCards();
 
+nextBtn.addEventListener('click', () => {
+  cardEl[currentActiveCard].className = 'card left';
+  currentActiveCard = currentActiveCard +1;
+  if (currentActiveCard > cardEl.length -1){  
+      currentActiveCard = cardEl.length -1;
+  }
+  cardEl[currentActiveCard].className = 'card active';
+  updateCurrentText();
+});
+prevBtn.addEventListener('click', () => {
+  cardEl[currentActiveCard].className = 'card right';
+  currentActiveCard = currentActiveCard - 1;
+  if (currentActiveCard < 0){  
+      currentActiveCard = 0;
+  }
+  cardEl[currentActiveCard].className = 'card active';
+  updateCurrentText();
+})
 
